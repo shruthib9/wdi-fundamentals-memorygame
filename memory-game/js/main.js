9ad86671bab0eb468bdb08d8537eb56ae8f45731
Shruthi <shrurepbog@gmail.com>
2020-04-20 23:55:20 -0600

    changes made for objects module

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,25 +1,33 @@
 console.log("Up and running!");
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+    {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+    {rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+    {rank: "king", suit: "diamonds", cardImage: "iimages/king-of-diamonds.png"}
+];
+
 let cardsInPlay = [];
 let cardId;
 
 function checkForMatch() {
     if (cardsInPlay.length === 2) {
 
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} 
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+        } 
         else {
-  console.log("Sorry, try again.");
-}
+          alert("Sorry, try again.");
+        }
     }
 }
+
 function flipCard(cardId){
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-checkForMatch();
- 
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    checkForMatch();
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 }
 
-flipCard(1);
+flipCard(0);
 flipCard(2);
