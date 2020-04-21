599582276610b8630d371dd5530e39cb2174024e
Shruthi <shrurepbog@gmail.com>
2020-04-21 16:01:08 -0600

    Changes made for DOM

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,10 +1,9 @@
 console.log("Up and running!");
-let cards = [
-    {rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+let cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
     {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
     {rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
-    {rank: "king", suit: "diamonds", cardImage: "iimages/king-of-diamonds.png"}
-];
+    {rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
+
 
 let cardsInPlay = [];
 let cardId;
@@ -21,13 +20,46 @@ function checkForMatch() {
     }
 }
 
-function flipCard(cardId){
-    console.log("User flipped " + cards[cardId].rank);
+function flipCard(){
+    cardId = this.getAttribute('data-id');
+//    this.setAttribute('src', this.getAttribute('cardImage'));
+    this.setAttribute('src', cards[cardId].cardImage);
+    console.log("User flipped " + cards[cardId].rank);    
     cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	let cardElementList = [];
+    
+    for (var i = cards.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+    
+//    for(var i=0; i<cards.length; i++){
+//        console.log(cards[i].cardImage)
+//    }
+    
+	for (let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        //cardElement.setAttribute('cardImage', cards[i].cardImage);
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+function resetBoard(){
+    document.getElementById('game-board').innerHTML = "";
+    cardsInPlay = []; 
+    createBoard();
+}
+
+
+createBoard();
